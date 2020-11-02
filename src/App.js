81bc2642708ed9a58b04aed485ebdcf6e43afd90
ursa <ursa.maciejewska@gmail.com>
2020-11-02 17:53:23 +0100

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainPage />
   );
 }
+
+const MainPage = () => {
+  return (
+    <div class="wrapper">
+      <div class="nav">
+        <div class="logo">LWK</div>
+      </div>
+      <div class="header-text">
+        <h1>Lot w Kosmos</h1>
+        <p>Nowy wymiar podróży.</p>
+        <button type="button">WIĘCEJ</button>
+      </div>
+      <div class="footer">
+        <div class="footer-text">Lot w Kosmos, ul. Wiejska 45A, 15-351 Białystok</div>
+      </div>
+    </div>
+  );
+} 
 
 export default App;
