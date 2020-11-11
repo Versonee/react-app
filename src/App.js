bbb68f98aab5201b4a50f1077082d02dab9c26d8
ursa <ursa.maciejewska@gmail.com>
2020-11-11 13:00:32 +0100

    Drobne sprzątanie struktury plików

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import MainView from "./MainView";
 import React, {Component} from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MainView from "./components/MainView";
 import PlanetView from "./components/PlanetView";
+import FlightsView from "./components/FlightsView";
 import './css/style.css'
 
 class App extends Component{
-
     render(){
         return(
             <Router>
                 <Route exact path="/" render={()=><MainView/>}/>
                 <Route exact path="/planets" render={()=><PlanetView/>}/>
+                <Route exact path="/flights" render={()=><FlightsView/>}/>
             </Router>
         );
     }
---
+++ src/components/FlightsView.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import '../css/FlightsView.css';
+
+class FlightsView extends Component {
+	render() {
+		return (
+			<div>loty</div>
+		)
+	}
+}
+
+export default FlightsView;
