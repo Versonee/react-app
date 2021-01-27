2c6fd69c9dce31cd701a367942b68125daff6c83
Mateusz Bielecki <bielakb98@gmail.com>
2021-01-27 08:50:20 +0100

    changed navigation, handled button click in main view

--- src/components/views/MainView.js
+++ src/components/views/MainView.js
@@ -1,9 +1,13 @@
 import React from "react";
 import '../../css/MainView.css';
 import Navigation from "../common/Navigation";
-
+import {useHistory} from "react-router";
 
 const MainView = () => {
+    const history = useHistory();
+    const handleClick = ()=>{
+        history.push('/planets');
+    }
     return (
         <>
             <Navigation toggleable={0}/>
@@ -15,7 +19,7 @@ const MainView = () => {
                 <div className="header-text">
                     <h1>Lot w Kosmos</h1>
                     <p>Nowy wymiar podróży.</p>
-                    <button type="button">WIĘCEJ</button>
+                    <button onClick={()=>handleClick()} type="button">WIĘCEJ</button>
                 </div>
                 <div className="footer">
                     <div className="footer-text">Lot w Kosmos, ul. Wiejska 45A, 15-351 Białystok</div>
