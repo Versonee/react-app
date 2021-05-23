f4d0556a92c5449a34e8720c5a3f7decda03eddf
ursa <ursa.maciejewska@gmail.com>
2021-05-23 02:14:20 +0200

    Wyświetlanie historii transakcji w podstronie profilu

--- src/components/views/ProfileView.js
+++ src/components/views/ProfileView.js
@@ -3,6 +3,36 @@ import React, {useState} from "react";
 import {useHistory} from "react-router";
 import ShoppingCart from "./ShoppingCart";
 import '../../css/ProfileView.css';
+import axios from "axios";
+
+function getPreviousUserFlights() {
+    var user = window.sessionStorage.getItem("user");
+    user = JSON.parse(user);
+
+    if (user.id) {
+        axios.get("http://bakent.herokuapp.com/tickets/user/" + user.id)
+                .then(res => {
+                    var display = "";
+                    if (res.data !== null && res.data !== "" && res.data.length !== 0) {
+                        display = '<div class="shopping-cart"><div class="shop-items">'
+                        res.data.forEach((item) => {
+                            console.log(item);
+                            display += '<div class="item"><div class="arrow">z</div>'+
+                            '<div class="from">'+item.flight.portLink.startingPort.name+', '+item.flight.portLink.startingPort.planet.name+'</div>'+
+                            '<div class="arrow">do</div>'+
+                            '<div class="to">'+item.flight.portLink.endPort.name+', '+item.flight.portLink.endPort.planet.name+'</div>'+
+                            '<div class="arrow">wylot</div>'+
+                            '<div class="price">'+item.flight.start_date+'</div>'+
+                            '</div>'
+                        });
+                        display += "</div></div>";
+                    } else {
+                        display = "Brak poprzednich transakcji.";
+                    }
+                    document.getElementById("previous-transactions").innerHTML = display;
+                });
+    }
+}
 
 export default function ProfileView(props){
     const [user] = useState(JSON.parse(window.sessionStorage.getItem("user")) || props.user);
@@ -27,7 +57,7 @@ export default function ProfileView(props){
                         <ShoppingCart removeTrigger={props.removeTrigger} shopList={props.shopList}/>
                         <div className="history">
                             <div className="description">Historia transakcji</div>
-                            <div className="empty-message">Brak poprzednich transakcji.</div>
+                            <div id="previous-transactions">{getPreviousUserFlights()}Brak poprzednich transakcji.</div>
                         </div>
                     </div>
                 </div>
